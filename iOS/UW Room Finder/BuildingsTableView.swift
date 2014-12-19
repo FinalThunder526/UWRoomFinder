247e74ec67dace2e825c659f0b4359ca970cc206
rajasagashe <rajasagashe@gmail.com>
2014-12-18 17:25:26 -0800

    BuildingNames
    The names of the buildings are displayed in a table

--- iOS/UW Room Finder/BuildingsTableView.swift
+++ iOS/UW Room Finder/BuildingsTableView.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class BuildingsTableView: UITableViewController {
 
+    var buildingNames:NSMutableArray = NSMutableArray()
+    
     override func viewDidLoad() {
         self.title = "Buildings"
+        let building = PFObject(className:"StudyRoom")
+        building["buildingName"] = "Lander"
+        building["description"] = "In da Club"
+        building.save()
+        var query = PFQuery(className: "StudyRoom")
+        var array:NSArray = query.findObjects() as NSArray
+        for(var i = 0; i < array.count; i++){
+            var name:String = (array.objectAtIndex(i))["buildingName"] as String
+            buildingNames.addObject(name)
+        }
+        /*
+        var gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground()
+        */
     }
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return buildingNames.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -23,7 +42,7 @@ class BuildingsTableView: UITableViewController {
             cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "cellIdentifier")
         }
         
-        cell!.textLabel.text = "happy"
+        cell!.textLabel.text = buildingNames.objectAtIndex(indexPath.row) as String
         return cell!
     }
     
--- iOS/UW Room Finder/UW Room Finder/UWRoomFinderBridgingHeader.m
+++
@@ -1,9 +0,0 @@
-//
-//  UWRoomFinderBridgingHeader.m
-//  UW Room Finder
-//
-//  Created by Rajas Agashe on 12/18/14.
-//  Copyright (c) 2014 Rajas Agashe. All rights reserved.
-//
-
-#import <Parse/Parse.h>
