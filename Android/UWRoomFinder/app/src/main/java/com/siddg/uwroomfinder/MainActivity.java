88d9b3f419bf44d701611990f5130b0cd30dcb81
siddthesciencekid <sgorti3@uw.edu>
2014-12-18 19:23:43 -0800

    Building Page Created Android
    the first page was created. Refresh button also confirmed to work

--- Android/UWRoomFinder/app/src/main/java/com/siddg/uwroomfinder/MainActivity.java
+++ Android/UWRoomFinder/app/src/main/java/com/siddg/uwroomfinder/MainActivity.java
@@ -1,12 +1,32 @@
 package com.siddg.uwroomfinder;
 
+import android.app.ProgressDialog;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView buildingList;
+    private List<String> buildings;
+    private ArrayAdapter<String> buildingAdapter;
+    private ProgressDialog loadingFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +34,43 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         // App entry
-        foo();
+        buildingList = (ListView) findViewById(R.id.buildingList);
+        buildings = new ArrayList<String>();
+        buildingAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, buildings);
+        buildingList.setAdapter(buildingAdapter);
+        getBuildings();
+
+
     }
 
-    private void foo() {
+    private void getBuildings() {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("StudyRoom");
+        loadingFeedback = ProgressDialog.show(this, "", "Loading UW Buildings");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if(e == null) {
+                    Set<String> Buildings = new TreeSet<String>();
+                    for(ParseObject object: parseObjects) {
+                        String build = object.getString("buildingName");
+                        Buildings.add(build);
+                    }
+                    addBuildingstoList(Buildings);
+                } else {
+                    // Failed result
+                    Toast.makeText(MainActivity.this, "Failed to load building list", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
+    private void addBuildingstoList(Set<String> Buildings) {
+        buildings.clear();
+        for(String s : Buildings) {
+            buildings.add(s);
+        }
+        buildingAdapter.notifyDataSetChanged();
+        loadingFeedback.dismiss();
     }
 
     @Override
@@ -35,10 +87,11 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Refresh action
+        if(id == R.id.action_refresh) {
+            getBuildings();
         }
+
 
         return super.onOptionsItemSelected(item);
     }
