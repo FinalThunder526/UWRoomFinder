aa6eb14e647c7d2de21565a25f438108695cac20
Sarang Joshi <sarangj@msn.com>
2014-12-18 20:00:57 -0800

    Basic building list created

---
+++ WP/UWRoomFinder/UWRoomFinder/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using UWRoomFinder.Resources;
+using Parse;
+using System.Collections;
+
+namespace UWRoomFinder
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+            GetBuildings();
+        }
+
+        private async void GetBuildings()
+        {
+            // Queries the 
+            var query = ParseObject.GetQuery("StudyRoom");
+            IEnumerable<ParseObject> results = await query.FindAsync();
+            ItemCollection x = BuildingList.Items;
+            x.Clear();
+            foreach (ParseObject o in results)
+            {
+                string b = (string)o["buildingName"];
+                if (!x.Contains(b))
+                    x.Add(b);
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
